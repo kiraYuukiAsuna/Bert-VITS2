ae86f4127e93c1d32fe8cfc08b8375cafc5ae8b6
kiraYuukiAsuna <kirayuukiasuna@outlook.com>
2024-09-05 16:25:59 +0800

    Update.

--- export_onnx.py
+++ export_onnx.py
@@ -2,8 +2,8 @@ from onnx_modules import export_onnx
 import os
 
 if __name__ == "__main__":
-    export_path = "BertVits2.2PT"
-    model_path = "dataset/Elysia/models/G_78000.pth"
+    export_path = "Elysia_145000"
+    model_path = "dataset/Elysia/models/G_145000.pth"
     config_path = "dataset/Elysia/config.json"
     novq = False
     dev = False
@@ -11,4 +11,4 @@ if __name__ == "__main__":
         os.makedirs("onnx")
     if not os.path.exists(f"onnx/{export_path}"):
         os.makedirs(f"onnx/{export_path}")
-    export_onnx(export_path, model_path, config_path, novq, dev)
+    export_onnx(export_path, model_path, config_path, novq, dev, "chinese")
